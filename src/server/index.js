27c0aaaf9ff986eeefb4252d1b547a178c37d17f
Benjamin Goh <masotime@gmail.com>
2017-03-05 13:12:56 -0800

    Enable hot-reloading of express routes

--- src/server/index.js
+++ src/server/index.js
@@ -4,22 +4,15 @@ import { PORT } from 'common/constants';
 // bundling of bundle.js
 import bundler from 'server/bundler';
 
-// React stuff
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import App from 'components';
+// hot-reloading of express routes
+import initWatcher from 'server/watcher';
 
 const app = express();
 
 app.use(bundler());
 
-app.get('/', (req, res) => {
-	const model = {
-		title: 'base-react-app',
-		message: 'Hello Captain Anonymous!'
-	};
-
-	res.status(200).end(`<!DOCTYPE HTML>${renderToString(<App model={model} />)}`);
-});
+// here we use a require statement instead of imports to enable hot reloading
+initWatcher(require.resolve('server/router'));
+app.use('/', (req, res, next) => require('server/router').default(req, res, next));
 
 app.listen(PORT, () => console.log(`✅  Web server started at http://localhost:${PORT}`));
